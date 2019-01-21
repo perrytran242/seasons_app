f9093475b93988757bb5a5bdfb49929bc2452016
Perry Tran <ptran209@gmail.com>
2019-01-20 19:56:27 -0800

    refactored the seasons ternerary

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr it is cold',
+        iconName: 'snowflake'
+    },
+}
+
 const getSeason = (lat, month) => {
     if ( month > 2 && month < 9) {
-        return lat > 0 ? 'Summer' : 'Winter';
+        return lat > 0 ? 'summer' : 'winter';
     } else {
-        return lat > 0 ? 'Winter' : 'Summer';
+        return lat > 0 ? 'winter' : 'summer';
     }
 }
 
 export default (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
+    console.log(seasonConfig[season])
 
     return (
         <div>
-            { season === 'Winter' ? 'Burrr, It is chilly' : 'Lets hit the beach' }
+            <i className={`${iconName} icon`} ></i>
+            <h1>{ season === 'winter' ? 'Burrr, It is chilly' : 'Lets hit the beach' }</h1>
+            <i className={`${iconName} icon`} ></i>
         </div>
     )
 }
